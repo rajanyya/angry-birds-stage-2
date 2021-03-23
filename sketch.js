00999777a508d28ef9856ba6b7f09d958e5ef4fd
Rajanyya <rajanyya.majumdar1364@gmail.com>
2021-03-23 20:37:06 +0530

    made the first change!

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg;
+var backgroundImg,platform,homework;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,6 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+platform = new Ground(150,300,300,200)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -35,26 +36,31 @@ function setup(){
 
     bird = new Bird(100,100);
 
+
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
+
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
+
     box1.display();
     box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
     box3.display();
     box4.display();
-    pig3.display();
-    log3.display();
-
     box5.display();
+
+    ground.display();
+platform.display();
+
+    pig1.display();
+    pig3.display();
+
+    log1.display();
+    log3.display();
     log4.display();
     log5.display();
 
